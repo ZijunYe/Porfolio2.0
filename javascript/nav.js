113ef46a2e0cb8436be7d17755cb3f6c01208b1c
Zijun Ye <zijunye001@gmail.com>
2022-01-02 15:55:31 -0500

    aboutme page finished

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,5 +1,5 @@
 window.onscroll = () => {
-  const nav = document.querySelector('#navbar');
+  const nav = document.querySelector('#nav-items');
   if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
 };
-//using appear functionality 
+//using appear functionality
